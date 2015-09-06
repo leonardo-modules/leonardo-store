b2c5b11f771a23029111262548fb0384064c89f6
Michael Kutý <6du1ro.n@gmail.com>
2015-09-06 15:08:17 +0200

    override checkout

--- leonardo_store/shipping/utils.py
+++ leonardo_store/shipping/utils.py
@@ -3,4 +3,6 @@ def set_model_repository():
     '''set ModelRepository as default Shipping Repository'''
     from leonardo_store.shipping.repository import ModelRepository
     from oscar.apps.shipping import repository
+    from oscar.apps.checkout import views
     repository.Repository = ModelRepository
+    views.Repository = ModelRepository
